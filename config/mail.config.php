398c73af74539345d816a3227624438763e9cc2c
unteem <timothee@unteem.org>
2018-01-16 15:25:48 +0100

    add mail.config.php

---
+++ config/mail.config.php
@@ -0,0 +1,14 @@
+<?php
+$CONFIG = array (
+  'mail_domain' => getenv('MAIL_DOMAIN'),
+  'mail_from_address' => getenv('MAIL_DOMAIN'),
+  'mail_smtpmode' => 'smtp',
+  'mail_smtphost' => getenv('MAIL_HOST'),
+  'mail_smtpport' => getenv('MAIL_PORT'),
+  'mail_smtpauth' => 1,
+  'mail_smtpauthtype' => 'LOGIN',
+  'mail_smtpname' => getenv('MAIL_DOMAIN'),
+  'mail_smtppassword' => getenv('MAIL_PASS'),
+  'mail_smtpsecure' => 'tls',
+);
+
